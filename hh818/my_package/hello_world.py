d13479be834074abc0dd254929d1ad5f54659231
hh818 <hh818@nyu.edu>
2015-02-25 08:54:53 -0500

    hh818 assignment3

---
+++ hh818/my_package/hello_world.py
@@ -0,0 +1,15 @@
+'''
+Created on Feb 25, 2015
+
+@author: ds-ga-1007
+'''
+
+def hello_world():
+    print "Hello World!"
+
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys
+    hello_world()
